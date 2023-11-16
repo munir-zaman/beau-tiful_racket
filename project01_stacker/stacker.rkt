cb1a98b3aed7f918892b3e5639756e28f490ea59
Munir Zaman <bijli.begum.bd@gmail.com>
2023-11-16 22:34:47 +0600

    stack

--- project01_stacker/stacker.rkt
+++ project01_stacker/stacker.rkt
@@ -9,24 +9,53 @@
 |#
 
 (define (read-syntax path port)
-    (define src-lines (port->lines port))
-    #|format datum... + -> (handle +) ~a is placeholder|#
-    (define src-datums (format-datums '(handle ~a) src-lines))
-    #|
-        "`" is a quasi-quote.. meaning you can substitute variables using ",".. 
-        and also substitute multiple variables using ",@"|#
-    (define module-datum `(module stacker-mod "stacker.rkt" ,@src-datums))
-    (datum->syntax #f module-datum))
+  (define src-lines (port->lines port))
+  #| format datum... + -> (handle +) ~a is placeholder |#
+  (define src-datums (format-datums '(handle ~a) src-lines))
+  #|
+        "`" is a quasi-quote.. meaning you can substitute variables using ","..
+        and also substitute multiple variables using ",@"
+   |#
+  (define module-datum
+    `(module stacker-mod "stacker.rkt"
+       ,@src-datums))
+  (datum->syntax #f module-datum))
 
 #|
 
-    We also need to export read-syntax so it’s avail­able outside this file. 
-    In Racket, all defi­n­i­tions are private by default. 
+    We also need to export read-syntax so it’s avail­able outside this file.
+    In Racket, all defi­n­i­tions are private by default.
     To make a defi­n­i­tion public, we use provide
 |#
 (provide read-syntax)
 
 (define-macro (stacker-module-begine HANDLE-EXPR ...)
-    #'(#%module-begin 'HANDLE-EXPR ...))
+              #'(#%module-begin HANDLE-EXPR ... (display (first stack))))
 
 (provide (rename-out [stacker-module-begine #%module-begin]))
+
+#| STACK |#
+(define stack empty)
+
+(define (pop-stack!)
+  (define arg (first stack))
+  (set! stack (rest stack))
+  arg)
+
+(define (push-stack! arg)
+  (set! stack (cons arg stack)))
+
+#|
+    We define our handle func­tion to take one argu­ment, called arg.
+    By declaring arg in brackets with a default value of #f, we make it an optional argu­ment.
+|#
+(define (handle [arg #f])
+  (cond
+    [(number? arg) (push-stack! arg)]
+    [(or (equal? + arg) (equal? * arg))
+     (define op-result (arg (pop-stack!) (pop-stack!)))
+     (push-stack! op-result)]))
+
+(provide handle)
+(provide +
+         *)
